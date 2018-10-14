f431f55b2d0886c05469b222bb2759d0485f7584
Unknown <kubase.se@gmail.com>
2018-10-14 16:09:18 +0200

    szkielet todense

--- src/lab1/SparseDataFrame.java
+++ src/lab1/SparseDataFrame.java
@@ -15,8 +15,54 @@ public class SparseDataFrame extends DataFrame {
 		for(int i=0;i<df.columns.size(); i++)
 		{
 			columns.add(new Column(df.columns.get(i).columnName,df.columns.get(0).columnType));
+			columns.set(i,df.columns.get(i));
 			columns.get(i).ColumnToCOOLColumn(hide);
 		}
 	}
+
+	public DataFrame ToDense()
+    {
+        String[] colnames = new String[columns.size()], coltypes = new String[columns.size()];
+
+        for(int i=0; i<colnames.length; i++)
+        {
+            colnames[i] = columns.get(0).columnName;
+            coltypes[i] = columns.get(0).columnType;
+        }
+
+        DataFrame densedf = new DataFrame(colnames, coltypes);
+        int currentIndex;
+
+        for(int i=0; i<columns.size();i++)
+        {
+            currentIndex = 0;
+            for(int j=0; j<columns.get(i).col.size(); j++)
+            {
+                while(currentIndex < columns.get(i).col.get(j).GetIndex())
+                {
+                    densedf.columns.get(i).col.add(hide);
+                    currentIndex++;
+                }
+                densedf.columns.get(i).col.add(columns.get(i).col.get(j).GetValue());
+            }
+        }
+
+        int longest = 0, x;
+        for(int i=0; i<densedf.columns.size();i++)
+        {
+            if(densedf.columns.get(i).col.size() > longest)
+                longest = densedf.columns.get(i).col.size();
+        }
+
+        for(int i=0; i<densedf.columns.size();i++)
+        {
+            x = densedf.columns.get(i).col.size();
+            if(x < longest)
+                for(int j=1; j<=x; j++)
+                    densedf.columns.get(i).col.add(hide);
+        }
+
+        return densedf;
+    }
 
 }
