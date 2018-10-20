9f0d60d9fd936e7cc8fdab660c07d0bf0c47e8e7
Unknown <kubase.se@gmail.com>
2018-10-20 19:13:13 +0200

    Added all lab3 class except DateTimeValue

---
+++ src/lab1/StringValue.java
@@ -0,0 +1,92 @@
+package lab1;
+
+import java.util.Objects;
+
+public class StringValue extends Value
+{
+    public String body;
+
+    public StringValue(String x)
+    {
+        body=x;
+    }
+
+    public String toString()
+    {
+        return body;
+    }
+
+    public Value add(Value v)
+    {
+        return new StringValue(body+v.toString());
+    }
+
+    public  Value sub(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public  Value mul(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public  Value div(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+
+    }
+
+    public  Value pow(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public boolean eq(Value v)
+    {
+       return body.equals(v.toString());
+    }
+
+    public boolean lte(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public boolean lt(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public boolean gte(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public boolean gt(Value v)
+    {
+        throw new RuntimeException("Invalid operation for StringValue type");
+    }
+
+    public boolean neq(Value v)
+    {
+        return !this.eq(v);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        StringValue that = (StringValue) o;
+        return Objects.equals(body, that.body);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(body);
+    }
+
+    public Value create(String s)
+    {
+        return (new StringValue(s));
+    }
+}
--- src/lab1/Value.java
+++ src/lab1/Value.java
@@ -2,15 +2,20 @@ package lab1;
 
 public abstract class Value
 {
-
+    public Object body;
     public abstract String toString();
     public abstract Value add(Value v);
     public abstract Value sub(Value v);
-    public abstract Value eq(Value v);
-    public abstract Value lte(Value v);
-    public abstract Value gte(Value v);
-    public abstract Value neq(Value v);
+    public abstract Value mul(Value v);
+    public abstract Value div(Value v);
+    public abstract Value pow(Value v);
+    public abstract boolean eq(Value v);
+    public abstract boolean lte(Value v);
+    public abstract boolean lt(Value v);
+    public abstract boolean gte(Value v);
+    public abstract boolean gt(Value v);
+    public abstract boolean neq(Value v);
     public abstract boolean equals(Object other);
     public abstract int hashCode();
-    public abstract Object create(String s);
+    public abstract Value create(String s);
 }
