8da81add1c09d0307f30d691fb65434dadf798d4
Unknown <kubase.se@gmail.com>
2018-11-04 16:43:01 +0100

    All grouping methods done

--- src/lab1/GroupWrapper.java
+++ src/lab1/GroupWrapper.java
@@ -1,6 +1,7 @@
 package lab1;
 
 import java.util.HashMap;
+import java.util.Map;
 
 public class GroupWrapper implements Groupby
 {
@@ -40,12 +41,12 @@ public class GroupWrapper implements Groupby
         for (HashMap.Entry<Value, DataFrame> entry : group.entrySet())
         {
             result.columns.get(indexOfGroupingColumn).col.add(entry.getValue().columns.get(indexOfGroupingColumn).col.get(0));
-            for(int i=0;i<entry.getValue().Size();i++)
+            for(int i=0;i<entry.getValue().columns.size();i++)
             {
                 if(i==indexOfGroupingColumn)
                     continue;
                 maximum = entry.getValue().columns.get(i).col.get(0);
-                for(int j=1;j<entry.getValue().columns.get(i).col.size();j++)
+                for(int j=1;j<entry.getValue().Size();j++)
                 {
                     if(maximum.lt(entry.getValue().columns.get(i).col.get(j)))
                         maximum = entry.getValue().columns.get(i).col.get(j);
@@ -64,12 +65,12 @@ public class GroupWrapper implements Groupby
         for (HashMap.Entry<Value, DataFrame> entry : group.entrySet())
         {
             result.columns.get(indexOfGroupingColumn).col.add(entry.getValue().columns.get(indexOfGroupingColumn).col.get(0));
-            for(int i=0;i<entry.getValue().Size();i++)
+            for(int i=0;i<entry.getValue().columns.size();i++)
             {
                 if(i==indexOfGroupingColumn)
                     continue;
                 minimum = entry.getValue().columns.get(i).col.get(0);
-                for(int j=1;j<entry.getValue().columns.get(i).col.size();j++)
+                for(int j=1;j<entry.getValue().Size();j++)
                 {
                     if(minimum.gt(entry.getValue().columns.get(i).col.get(j)))
                         minimum = entry.getValue().columns.get(i).col.get(j);
@@ -82,11 +83,96 @@ public class GroupWrapper implements Groupby
 
     @Override public DataFrame mean()
     {
+        DataFrame result = this.DfCreator();
+        Value sum=null;
 
+        for (HashMap.Entry<Value, DataFrame> entry : group.entrySet())
+        {
+            result.columns.get(indexOfGroupingColumn).col.add(entry.getValue().columns.get(indexOfGroupingColumn).col.get(0));
+            for(int i=0;i<entry.getValue().columns.size();i++)
+            {
+                if(i==indexOfGroupingColumn)
+                    continue;
+                sum = entry.getValue().columns.get(i).col.get(0);
+                for(int j=1;j<entry.getValue().Size();j++)
+                {
+                    sum.add(entry.getValue().columns.get(i).col.get(j));
+                }
+                result.columns.get(i).col.add(sum.div(new IntegerValue(entry.getValue().Size())));
+            }
+        }
+        return result;
     }
 
-    @Override public DataFrame std(){}
-    @Override public DataFrame sum(){}
-    @Override public DataFrame var(){}
-    @Override public DataFrame apply(Applyable a){}
+    @Override public DataFrame std()
+    {
+        DataFrame result = this.var();
+
+        for(int i=0;i<result.columns.size();i++)
+        {
+            if(i==indexOfGroupingColumn)
+                continue;
+            for(int j=0;j<result.Size();j++)
+            {
+                result.columns.get(i).col.set(j,result.columns.get(i).col.get(j).pow(new DoubleValue(0.5)));
+            }
+        }
+        return result;
+    }
+
+    @Override public DataFrame sum()
+    {
+        DataFrame result = this.DfCreator();
+        Value sum=null;
+
+        for (HashMap.Entry<Value, DataFrame> entry : group.entrySet())
+        {
+            result.columns.get(indexOfGroupingColumn).col.add(entry.getValue().columns.get(indexOfGroupingColumn).col.get(0));
+            for(int i=0;i<entry.getValue().columns.size();i++)
+            {
+                if(i==indexOfGroupingColumn)
+                    continue;
+                sum = entry.getValue().columns.get(i).col.get(0);
+                for(int j=1;j<entry.getValue().Size();j++)
+                {
+                    sum.add(entry.getValue().columns.get(i).col.get(j));
+                }
+                result.columns.get(i).col.add(sum);
+            }
+        }
+        return result;
+    }
+
+    @Override public DataFrame var()
+    {
+        DataFrame means = this.mean();
+        DataFrame result = this.DfCreator();
+        Value sum=null;
+        int mRowIdx=0;
+
+        for (HashMap.Entry<Value, DataFrame> entry : group.entrySet())
+        {
+            result.columns.get(indexOfGroupingColumn).col.add(entry.getValue().columns.get(indexOfGroupingColumn).col.get(0));
+            for(int i=0;i<entry.getValue().columns.size();i++)
+            {
+                if(i==indexOfGroupingColumn)
+                    continue;
+                sum = entry.getValue().columns.get(i).col.get(0);
+                for(int j=1;j<entry.getValue().Size();j++)
+                {
+                    sum.add(((entry.getValue().columns.get(i).col.get(j)).sub(means.columns.get(i).col.get(mRowIdx))).pow(new IntegerValue(2)));
+                }
+                result.columns.get(i).col.add(sum.div(new IntegerValue(entry.getValue().Size())));
+            }
+            mRowIdx++;
+        }
+        return result;
+    }
+
+    @Override public DataFrame apply(Applyable a)
+    {
+        //???
+        DataFrame result = this.DfCreator();
+        return a.apply(result);
+    }
 }
